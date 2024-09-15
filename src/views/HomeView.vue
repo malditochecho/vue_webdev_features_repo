7de2c57a388b5041e328093c7b355fb17d2ce3a1
sergio <sergio_alberto.rpdriguez_ortiz@lcieducation.net>
2024-09-15 10:01:40 -0400

    tailwind installed and implemented

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main
+    class="flex min-h-svh min-w-full flex-col items-center justify-center bg-gradient-to-r from-emerald-500 to-emerald-900"
+  >
+    <h1 class="text-center text-7xl font-extrabold text-white drop-shadow-lg">Hello Tailwind!</h1>
   </main>
 </template>
